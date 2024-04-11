d22e3e461344c89a58be459e5fb9c643cb042310
Sabrina A <sabrinaangelle@gmail.com>
2024-04-11 11:56:29 +0900

    updated index.js file

--- index.js
+++ index.js
@@ -2,4 +2,36 @@
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
 
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const app = express();
+const port = 3000;
+const password = 'ILoveProgramming';
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true}))
+
+
+// Handlers
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+app.post('/check', (req, res) => {
+
+    if (req.body.password === password) {
+        res.sendFile(__dirname + "/public/secret.html");
+    } else {
+        res.sendFile(__dirname + "/public/index.html");
+    }
+
+    console.log(req.body.password)
+})
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}`);
+})
